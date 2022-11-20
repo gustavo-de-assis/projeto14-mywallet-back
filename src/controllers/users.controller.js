6a45c1c1e8c034becf498cb0757936b676e48727
gustavo de assis <gustavo.siqueira.421@gmail.com>
2022-11-20 03:59:51 -0300

    feat: rota para signin(usercontroller)

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,6 +9,7 @@ export async function signUpUser(req, res){
     try{
         const user = await usersCollection.findOne({email: newUser.email});
         
+        //User already exists...
         if(user){
             return res.status(409).send("User already exists!")
         }
@@ -22,4 +23,36 @@ export async function signUpUser(req, res){
         console.log(err);
         res.sendStatus(500);
     }
+}
+
+export async function signInUser(req, res) {
+    const { email, password } = req.body;
+    const token = uuidV4();
+
+    try {
+        const user = await usersCollection.findOne({ email });
+        //User not found!
+        if (!user) {
+            return res.sendStatus(401);
+        }
+
+        const validatePassword = bcrypt.compareSync(password, user.password);
+
+        //Wrong password!
+        if (!validatePassword) {
+            return res.sendStatus(401);
+        }
+
+        await sessionsCollection.insertOne({
+            token,
+            userId: user._id
+        })
+
+        res.send({ token })
+
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
+
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
-import { signUpUser } from "./controllers/users.controller.js";
+import { signInUser, signUpUser } from "./controllers/users.controller.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.post("/sign-up", signUpUser);
+app.post("/sign-in", signInUser);
 
 app.listen(5000, console.log("Server running at port 5000"));
