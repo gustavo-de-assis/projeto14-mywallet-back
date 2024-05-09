9442ca025a1e5b2050cbf6b76f4fef00eefa56f5
gustavo de assis <gustavo.siqueira.421@gmail.com>
2024-05-09 12:57:27 -0300

    feat: find user transactions method on repository

--- src/repositories/transaction.repository.js
+++ src/repositories/transaction.repository.js
@@ -3,3 +3,11 @@ import { transactionsCollection } from "../database/db.js";
 async function insertTransaction(data) {
   await transactionsCollection.insertOne({ ...data });
 }
+
+async function findUserTransactions(userId) {
+  const transactions = await transactionsCollection
+    .find({ userId: userId })
+    .toArray();
+
+  return transactions;
+}
